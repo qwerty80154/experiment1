e30578fee732788dbda018481dab85d77945c803
qwerty <mfcknglsngt@gmail.com>
2026-04-20 11:44:32 +0100

    Create Human-Attempt.bat

---
+++ AI test/Human-Attempt.bat
@@ -0,0 +1,14 @@
+@echo off
+echo This is the only non-AI.
+pause
+clear
+
+echo  _______ _______
+echo |       |       |
+echo |       |       |
+echo |_______ _______|
+echo |       |       |
+echo |       |       |
+echo |_______ _______|
+
+pause
