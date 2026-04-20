83eb5ba3da8569eef47877454e107e387c3e6114
qwerty <mfcknglsngt@gmail.com>
2026-04-20 11:30:56 +0100

    Create Grok-3-Attempt.bat
    this is the third code slop and still nothing

---
+++ AI test/Grok-3-Attempt.bat
@@ -0,0 +1,26 @@
+@echo off
+echo Blah blah blah, This code sucks, comtinue if you want to see garbage
+pause
+
+title Windows 11 Logo
+color 1F
+
+echo.
+echo.
+echo        ##########         ##########
+echo        #        #         #        #
+echo        #        #         #        #
+echo        #        #         #        #
+echo        ##########         ##########
+echo.
+echo.
+echo        ##########         ##########
+echo        #        #         #        #
+echo        #        #         #        #
+echo        #        #         #        #
+echo        ##########         ##########
+echo.
+echo.
+echo        Press any key to exit...
+pause >nul
+exit
